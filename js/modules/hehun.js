d1fa2cc0bcf0b4a1323087aa3805337f9ea59eba
xudq789 <616737126@qq.com>
2026-01-29 15:32:03 +0800

    Update hehun.js

--- js/modules/hehun.js
+++ js/modules/hehun.js
@@ -37,7 +37,7 @@ export class HehunModule {
 出生时间：${partnerData.partnerBirthYear}年${partnerData.partnerBirthMonth}月${partnerData.partnerBirthDay}日${partnerData.partnerBirthHour}时${partnerData.partnerBirthMinute}分
 出生城市：${partnerData.partnerBirthCity}
 
-请先进行八字排盘，大运排盘先确认出生当日离前后节气的日数，男命阳顺阴逆，女命阳逆阴顺，三日一年推算起运岁数，然后用以下格式输出结果：
+请先进行八字排盘，大运排盘（大运顺逆规则：阳男阴女顺行、阴男阳女逆行。起运岁数计算规则：顺行者，从出生日数至下一个节气，三日为一岁；逆行者，从出生日数至上一个节气，三日为一岁），然后用以下格式输出结果：
 
 【八字排盘】
 年柱：[年柱干支] ([纳音])
@@ -94,6 +94,7 @@ export class HehunModule {
         return calculateBazi(userData);
     }
 }
+
 
 
 
